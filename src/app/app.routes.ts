d2af63dd0be78a6970daaf90823c650428c2f2ac
laptop <masumahmed1438@gmail.com>
2025-10-23 17:09:24 +0600

    Added Assign Mentor feature (Issue #7)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { Mentor } from "./mentor-management/mentor/mentor";
+import { MentorAssign } from "./mentor-management/mentor-assign/mentor-assign";
+import { MentorProfile } from "./mentor-management/mentor-profile/mentor-profile";
+import { MentorReview } from "./mentor-management/mentor-review/mentor-review";
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: 'mentor', component: Mentor },
+    { path: 'assign', component: MentorAssign},
+  { path: 'profile/:id', component: MentorProfile },
+  { path: 'review/:id', component: MentorReview},
+];
