751a9b044bb9937fde7e9f2d5333dafffe72121b
mdarmanhossen <mdarmanhossen404@gmail.com>
2025-10-25 00:23:28 +0600

    student-management

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,14 @@ import { Mentor } from "./mentor-management/mentor/mentor";
 import { MentorAssign } from "./mentor-management/mentor-assign/mentor-assign";
 import { MentorProfile } from "./mentor-management/mentor-profile/mentor-profile";
 import { MentorReview } from "./mentor-management/mentor-review/mentor-review";
+import { Students } from "./student-management/student-management";
 
 
 export const routes: Routes = [
+    {path:'student', component:Students},
     { path: 'mentor', component: Mentor },
     { path: 'assign', component: MentorAssign},
-  { path: 'profile/:id', component: MentorProfile },
-  { path: 'review/:id', component: MentorReview},
+    { path: 'profile/:id', component: MentorProfile },
+    { path: 'review/:id', component: MentorReview},
+
 ];
